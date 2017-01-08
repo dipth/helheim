662dceb1525206e30da7f9357a0de52a001b2cae
Thomas Dippel <thomas@dippel.dk>
2017-01-08 23:18:34 +0100

    Add support for testing with Semaphore CI

--- config/test.exs
+++ config/test.exs
@@ -14,8 +14,8 @@ config :logger, level: :warn
 # Configure your database
 config :altnation, Altnation.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
   database: "altnation_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
