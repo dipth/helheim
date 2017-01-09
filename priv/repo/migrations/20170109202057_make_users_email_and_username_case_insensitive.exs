ee08ac96920609fac5aab77a1a0a40638fb31e15
Thomas Dippel <thomas@dippel.dk>
2017-01-09 21:49:54 +0100

    Make username and e-mail case insensitive

---
+++ priv/repo/migrations/20170109202057_make_users_email_and_username_case_insensitive.exs
@@ -0,0 +1,27 @@
+defmodule Altnation.Repo.Migrations.MakeUsersEmailAndUsernameCaseInsensitive do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;")
+    drop unique_index(:users, [:email])
+    drop unique_index(:users, [:username])
+    alter table(:users) do
+      modify :email, :citext, null: false
+      modify :username, :citext, null: false
+    end
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:username])
+  end
+
+  def down do
+    drop unique_index(:users, [:email])
+    drop unique_index(:users, [:username])
+    alter table(:users) do
+      modify :email, :string, null: false
+      modify :username, :string, null: false
+    end
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:username])
+    execute("DROP EXTENSION IF EXISTS citext;")
+  end
+end
--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -1,19 +1,48 @@
 defmodule Altnation.UserTest do
   use Altnation.ModelCase
+  alias Altnation.Repo
   alias Altnation.User
   import Altnation.Factory
 
   @valid_registration_attrs %{name: "Foo Bar", username: "foobar", email: "foo@bar.dk", password: "password"}
   @invalid_registration_attrs %{}
 
-  test "registration_changeset with valid attributes" do
-    changeset = User.registration_changeset(%User{}, @valid_registration_attrs)
-    assert changeset.valid?
-  end
+  describe "registration_changeset/2" do
+    test "it accepts valid attributes" do
+      changeset = User.registration_changeset(%User{}, @valid_registration_attrs)
+      assert changeset.valid?
+    end
 
-  test "registration_changeset with invalid attributes" do
-    changeset = User.registration_changeset(%User{}, @invalid_registration_attrs)
-    refute changeset.valid?
+    test "It rejects invalid attributes" do
+      changeset = User.registration_changeset(%User{}, @invalid_registration_attrs)
+      refute changeset.valid?
+    end
+
+    test "It rejects duplicate usernames regardles of case" do
+      %User{}
+      |> User.registration_changeset(@valid_registration_attrs)
+      |> Repo.insert!
+
+      user2 =
+        %User{}
+        |> User.registration_changeset(@valid_registration_attrs |> Map.merge(%{username: "FOOBAR", email: "something@else.dk"}))
+
+      assert {:error, changeset} = Repo.insert(user2)
+      assert {"has already been taken", _} = changeset.errors[:username]
+    end
+
+    test "It rejects duplicate e-mail addresses regardles of case" do
+      %User{}
+      |> User.registration_changeset(@valid_registration_attrs)
+      |> Repo.insert!
+
+      user2 =
+        %User{}
+        |> User.registration_changeset(@valid_registration_attrs |> Map.merge(%{username: "somethingelse", email: "FOO@BAR.DK"}))
+
+      assert {:error, changeset} = Repo.insert(user2)
+      assert {"has already been taken", _} = changeset.errors[:email]
+    end
   end
 
   describe "confirm!/1" do
