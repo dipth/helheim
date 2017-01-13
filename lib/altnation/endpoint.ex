8fd8d2e4d7d65c2f828d94714a3ee17eb3b41a36
Thomas Dippel <thomas@dippel.dk>
2017-01-13 22:35:29 +0100

    Add favicons

--- lib/altnation/endpoint.ex
+++ lib/altnation/endpoint.ex
@@ -14,7 +14,7 @@ defmodule Altnation.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :altnation, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js robots.txt android-chrome-192x192.png apple-touch-icon.png browserconfig.xml favicon-16x16.png favicon-32x32.png favicon.ico manifest.json mstile-150x150.png safari-pinned-tab.svg)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
