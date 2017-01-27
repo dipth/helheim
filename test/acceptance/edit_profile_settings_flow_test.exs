88bf2caf52bca9738fc6942d5852a2a94428cfb1
Thomas Dippel <thomas@dippel.dk>
2017-01-28 00:03:19 +0100

    Re-enable disabled test

--- test/acceptance/edit_profile_settings_flow_test.exs
+++ test/acceptance/edit_profile_settings_flow_test.exs
@@ -1,33 +1,26 @@
 defmodule Helheim.EditProfileSettingsFlowTest do
   use Helheim.AcceptanceCase, async: true
 
-  # TODO: Enable this test when you figure out how to use Wallaby with the
-  #       Trumbowyg editor:
-  #       https://github.com/keathley/wallaby/issues/129
-  # test "users can edit their profile settings", %{session: session} do
-  #   user = insert(:user)
-  #
-  #   session
-  #   |> visit("/sessions/new")
-  #   |> fill_in(gettext("E-mail"), with: user.email)
-  #   |> fill_in(gettext("Password"), with: "password")
-  #   |> click_on(gettext("Sign In"))
-  #
-  #   session
-  #   |> find(".nav-item-user-menu")
-  #   |> click_link(user.username)
-  #   |> click_link(gettext("Profile"))
-  #
-  #   session
-  #   |> fill_in(".trumbowyg-editor", with: "This is my profile text")
-  #   |> click_on(gettext("Update Profile"))
-  #
-  #   TODO: Also test the profile photo upload
-  #
-  #   result = session
-  #   |> find(".alert.alert-success")
-  #   |> text
-  #
-  #   assert result =~ gettext("Profile Updated!")
-  # end
+  setup [:create_and_sign_in_user]
+
+  test "users can edit their profile settings", %{session: session, user: user} do
+    session
+    |> find(".nav-item-user-menu")
+    |> click_link(user.username)
+    |> click_link(gettext("Profile"))
+
+    session
+    |> execute_script("$('#profile_text_editor .ql-editor').html('This is my awesome text');")
+
+    session
+    |> click_on(gettext("Update Profile"))
+
+    # TODO: Also test the profile photo upload
+
+    result = session
+    |> find(".alert.alert-success")
+    |> text
+
+    assert result =~ gettext("Profile updated!")
+  end
 end
