b384ee675811bd8508a696c29edbfc8ea0acb90f
Thomas Dippel <thomas@dippel.dk>
2017-02-26 21:18:29 +0100

    Refactor ForumTopicController to use plugs

--- web/controllers/forum_topic_controller.ex
+++ web/controllers/forum_topic_controller.ex
@@ -4,45 +4,54 @@ defmodule Helheim.ForumTopicController do
   alias Helheim.ForumTopic
   alias Helheim.ForumReply
 
-  def new(conn, %{"forum_id" => forum_id}) do
-    forum     = Forum |> preload(:forum_category) |> Repo.get!(forum_id)
-    user      = current_resource(conn)
-    changeset = forum
-                |> Ecto.build_assoc(:forum_topics)
-                |> ForumTopic.changeset
-                |> Ecto.Changeset.put_assoc(:user, user)
-    render(conn, "new.html", forum: forum, changeset: changeset)
+  plug :find_forum
+  plug :find_user
+  plug :build_forum_topic_changeset, except: [:show]
+
+  def new(conn, %{"forum_id" => _}) do
+    render(conn, "new.html")
   end
 
-  def create(conn, %{"forum_id" => forum_id, "forum_topic" => forum_topic_params}) do
-    forum     = Forum |> preload(:forum_category) |> Repo.get!(forum_id)
-    user      = current_resource(conn)
-    changeset = forum
-                |> Ecto.build_assoc(:forum_topics)
-                |> ForumTopic.changeset(forum_topic_params)
-                |> Ecto.Changeset.put_assoc(:user, user)
-
-    case Repo.insert(changeset) do
+  def create(conn, %{"forum_id" => _, "forum_topic" => _}) do
+    case Repo.insert(conn.assigns[:changeset]) do
       {:ok, forum_topic} ->
         conn
         |> put_flash(:success, gettext("Forum topic created successfully."))
-        |> redirect(to: forum_forum_topic_path(conn, :show, forum, forum_topic))
+        |> redirect(to: forum_forum_topic_path(conn, :show, conn.assigns[:forum], forum_topic))
       {:error, changeset} ->
-        render(conn, "new.html", forum: forum, changeset: changeset)
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def show(conn, %{"forum_id" => forum_id, "id" => id} = params) do
-    forum         = Forum |> preload(:forum_category) |> Repo.get!(forum_id)
-    forum_topic   = assoc(forum, :forum_topics) |> preload(:user) |> Repo.get!(id)
+  def show(conn, %{"forum_id" => _, "id" => id} = params) do
+    forum_topic   = assoc(conn.assigns[:forum], :forum_topics) |> preload(:user) |> Repo.get!(id)
     forum_replies = assoc(forum_topic, :forum_replies)
                     |> preload(:user)
                     |> Repo.paginate(page: sanitized_page(params["page"]))
-    user      = current_resource(conn)
-    changeset = forum_topic
-                |> Ecto.build_assoc(:forum_replies)
-                |> ForumReply.changeset()
-                |> Ecto.Changeset.put_assoc(:user, user)
-    render(conn, "show.html", forum: forum, forum_topic: forum_topic, forum_replies: forum_replies, changeset: changeset)
+    changeset     = forum_topic
+                    |> Ecto.build_assoc(:forum_replies)
+                    |> ForumReply.changeset()
+                    |> Ecto.Changeset.put_assoc(:user, conn.assigns[:user])
+    render(conn, "show.html", forum_topic: forum_topic, forum_replies: forum_replies, changeset: changeset)
+  end
+
+  defp find_forum(conn, _) do
+    forum_id = conn.params["forum_id"]
+    forum    = Forum |> preload(:forum_category) |> Repo.get!(forum_id)
+    assign conn, :forum, forum
+  end
+
+  defp find_user(conn, _) do
+    user = current_resource(conn)
+    assign conn, :user, user
+  end
+
+  defp build_forum_topic_changeset(conn, _) do
+    forum_topic_params = conn.params["forum_topic"] || %{}
+    changeset = conn.assigns[:forum]
+                |> Ecto.build_assoc(:forum_topics)
+                |> ForumTopic.changeset(forum_topic_params)
+                |> Ecto.Changeset.put_assoc(:user, conn.assigns[:user])
+    assign conn, :changeset, changeset
   end
 end
