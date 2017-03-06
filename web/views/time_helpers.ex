febc7c144dcd24ad1cdfd94e8dfa4f7f106cc980
Thomas Dippel <thomas@dippel.dk>
2017-03-06 22:28:03 +0100

    Fix timestamps

--- web/views/time_helpers.ex
+++ web/views/time_helpers.ex
@@ -2,11 +2,17 @@ defmodule Helheim.TimeHelpers do
   def lt(timestamp, format) do
     {:ok, string} = timestamp
       |> Calendar.DateTime.shift_zone!("Europe/Copenhagen")
-      |> Timex.lformat(format, "da")
+      |> Timex.lformat(format, "da", :strftime)
     string
   end
 
-  def time_ago_in_words(timestamp) do
-    timestamp |> Timex.from_now("da")
+  def time_ago_in_words(timestamp, opts \\ []) do
+    content = timestamp |> Timex.from_now("da")
+    if opts[:simple] == true do
+      content
+    else
+      title = lt(timestamp, "%d. %b %Y kl. %H:%M")
+      Phoenix.HTML.Tag.content_tag :span, content, title: title
+    end
   end
 end
