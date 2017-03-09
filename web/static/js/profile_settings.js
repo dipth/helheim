347a1d2ddcf676cfc97079e7257c99ad29157033
Thomas Dippel <thomas@dippel.dk>
2017-03-09 20:39:25 +0100

    Disable autolinking in profiletext and blogs

--- web/static/js/profile_settings.js
+++ web/static/js/profile_settings.js
@@ -5,11 +5,11 @@ export var ProfileSettings = {
       height: 350,
       menubar: false,
       plugins: [
-        'autolink lists link image charmap print preview anchor',
+        'lists link image charmap print preview anchor',
         'searchreplace visualblocks code fullscreen',
         'insertdatetime table contextmenu paste code'
       ],
-      toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | link image',
+      toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | link image'
     })
   },
 }
