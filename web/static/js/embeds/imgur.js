92b302d9363d903e15241cc8667698333cf5126d
Thomas Dippel <thomas@dippel.dk>
2017-03-11 01:04:39 +0100

    Support both older and newer imgur image ids

--- web/static/js/embeds/imgur.js
+++ web/static/js/embeds/imgur.js
@@ -1,9 +1,15 @@
 let regEx = /\[imgur\](.+?)\[\/imgur\]/gi
 
 let replacer = function(match, imageId) {
-  return `
-    <blockquote class="imgur-embed-pub" lang="en" data-id="a/${imageId}"><a href="//imgur.com/${imageId}">Imgur</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
-  `;
+  if (imageId.length == 5) {
+    return `
+      <blockquote class="imgur-embed-pub" lang="en" data-id="a/${imageId}"><a href="//imgur.com/${imageId}"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+    `;
+  } else {
+    return `
+      <blockquote class="imgur-embed-pub" lang="en" data-id="${imageId}"><a href="//imgur.com/${imageId}"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+    `;
+  }
 }
 
 export var ImgurEmbed = {
