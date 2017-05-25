30347c21bfdf60f50add96114764de86e2391783
Thomas Dippel <thomas@dippel.dk>
2017-05-25 11:13:37 +0200

    Fix notification bug
    Where the page would fail loading if an unread notification was present with a deleted trigger person

--- web/views/notification_view.ex
+++ web/views/notification_view.ex
@@ -19,21 +19,24 @@ defmodule Helheim.NotificationView do
   end
 
   defp notification_text(trigger_person, "comment", %User{} = profile) do
-    gettext "%{who} wrote a comment in the guest book: %{what}", who: trigger_person.username, what: profile.username
+    gettext "%{who} wrote a comment in the guest book: %{what}", who: trigger_person_name(trigger_person), what: profile.username
   end
   defp notification_text(trigger_person, "comment", %BlogPost{} = blog_post) do
-    gettext "%{who} wrote a comment on the blog post: %{what}", who: trigger_person.username, what: blog_post.title
+    gettext "%{who} wrote a comment on the blog post: %{what}", who: trigger_person_name(trigger_person), what: blog_post.title
   end
   defp notification_text(trigger_person, "comment", %PhotoAlbum{} = photo_album) do
-    gettext "%{who} wrote a comment on the photo album: %{what}", who: trigger_person.username, what: photo_album.title
+    gettext "%{who} wrote a comment on the photo album: %{what}", who: trigger_person_name(trigger_person), what: photo_album.title
   end
   defp notification_text(trigger_person, "comment", %Photo{} = photo) do
-    gettext "%{who} wrote a comment on the photo: %{what}", who: trigger_person.username, what: photo.title
+    gettext "%{who} wrote a comment on the photo: %{what}", who: trigger_person_name(trigger_person), what: photo.title
   end
   defp notification_text(trigger_person, "forum_reply", %ForumTopic{} = forum_topic) do
-    gettext "%{who} wrote a reply to the forum topic: %{what}", who: trigger_person.username, what: forum_topic.title
+    gettext "%{who} wrote a reply to the forum topic: %{what}", who: trigger_person_name(trigger_person), what: forum_topic.title
   end
 
   defp notification_icon("comment"), do: "fa fa-fw fa-comment-o"
   defp notification_icon("forum_reply"), do: "fa fa-fw fa-comments-o"
+
+  defp trigger_person_name(nil), do: gettext("User deleted")
+  defp trigger_person_name(trigger_person), do: trigger_person.username
 end
