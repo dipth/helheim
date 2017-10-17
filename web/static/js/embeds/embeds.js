36d62f054c91df0e4063442b032304e1ac565613
Thomas Dippel <thomas@dippel.dk>
2017-10-17 23:04:16 +0200

    Working on better embeds

--- web/static/js/embeds/embeds.js
+++ web/static/js/embeds/embeds.js
@@ -4,15 +4,33 @@ import { GiphyEmbed } from "./giphy";
 import { ImgurEmbed } from "./imgur";
 import { SoundCloudEmbed } from "./soundcloud";
 
+import EmbedJS from 'embed-js';
+import url from 'embed-plugin-url';
+import noembed from 'embed-plugin-noembed';
+
 export var Embeds = {
   run: function(){
+    window.process = {}
+
     $('.embeds').each(function(index){
-      let el  = $(this);
-      YoutubeEmbed.run(el);
-      VimeoEmbed.run(el);
-      GiphyEmbed.run(el);
-      ImgurEmbed.run(el);
-      SoundCloudEmbed.run(el);
+      let el = $(this);
+      let x = new EmbedJS({
+        input: el[0],
+        plugins: [
+          url(),
+          noembed({})
+        ],
+        replaceUrl: true
+      });
+
+      x.render();
+
+      // Legacy
+      // YoutubeEmbed.run(el);
+      // VimeoEmbed.run(el);
+      // GiphyEmbed.run(el);
+      // ImgurEmbed.run(el);
+      // SoundCloudEmbed.run(el);
     })
   },
 }
