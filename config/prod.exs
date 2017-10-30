b6abe154abc25a5da2fd0a582d95399573258c96
Thomas Dippel <thomas@dippel.dk>
2017-10-30 16:37:14 +0100

    Fix `Could not check origin` error

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,8 @@ config :helheim, Helheim.Endpoint,
   url: [scheme: "https", host: "helheim.dk", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: ["//helheim.dk", "//www.helheim.dk"]
 
 # Configure your database
 config :helheim, Helheim.Repo,
