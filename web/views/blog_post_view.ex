6144dfe22a25b4b3edded3e5284b095eeb1f9452
Thomas Dippel <thomas@dippel.dk>
2017-11-13 23:41:37 +0100

    Also show edited label on show page of blog post

--- web/views/blog_post_view.ex
+++ web/views/blog_post_view.ex
@@ -1,3 +1,16 @@
 defmodule Helheim.BlogPostView do
   use Helheim.Web, :view
+  alias Helheim.BlogPost
+
+  def edited_label(blog_post) do
+    if BlogPost.edited?(blog_post) do
+      content_tag :span, class: "badge badge-default" do
+        [
+          content_tag(:i, "", class: "fa fa-fw fa-pencil"),
+          {:safe, [" "]},
+          {:safe, [gettext("Edited")]}
+        ]
+      end
+    end
+  end
 end
