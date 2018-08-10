1e67c1028f90d33057a79ba416d44d300e6552f8
Thomas Dippel <thomas@dippel.dk>
2018-08-10 23:23:56 +0200

    Add embed hints to forms
    and remove old embed help-page

--- web/controllers/help_controller.ex
+++ web/controllers/help_controller.ex
@@ -2,10 +2,6 @@ defmodule Helheim.HelpController do
   use Helheim.Web, :controller
   alias Helheim.User
 
-  def embeds(conn, _params) do
-    render conn, "embeds.html"
-  end
-
   def verification(conn, _params) do
     user = current_resource(conn)
     verified = User.verified?(user)
