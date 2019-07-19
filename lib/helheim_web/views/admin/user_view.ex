138c22d30c05a09f8d8fb34661647c40d714d0a9
Thomas Dippel <dipth@users.noreply.github.com>
2019-07-19 13:14:17 +0200

    Fix bug when viewing profile
    that has comments on calendar events

--- lib/helheim_web/views/admin/user_view.ex
+++ lib/helheim_web/views/admin/user_view.ex
@@ -14,6 +14,7 @@ defmodule HelheimWeb.Admin.UserView do
   def comment_path(conn, %Helheim.User{} = profile), do: public_profile_comment_path(conn, :index, profile)
   def comment_path(conn, %Helheim.BlogPost{} = blog_post), do: public_profile_blog_post_path(conn, :show, blog_post.user_id, blog_post)
   def comment_path(conn, %Helheim.Photo{} = photo), do: public_profile_photo_album_photo_path(conn, :show, photo.photo_album.user_id, photo.photo_album_id, photo)
+  def comment_path(conn, %Helheim.CalendarEvent{} = event), do: calendar_event_path(conn, :show, event)
 
   def block_user_link(_conn, current_user, user) when current_user == user, do: user.username
   def block_user_link(conn, _current_user, user), do: link(user.username, to: admin_user_path(conn, :show, user))
