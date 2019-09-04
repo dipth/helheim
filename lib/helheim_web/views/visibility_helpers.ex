f654f6431dd6908672d903f6a13ab9ae2debaa8f
Thomas Dippel <thomas@dippel.dk>
2019-09-04 17:38:14 +0200

    Tone down the visibility badge

--- lib/helheim_web/views/visibility_helpers.ex
+++ lib/helheim_web/views/visibility_helpers.ex
@@ -5,7 +5,7 @@ defmodule HelheimWeb.VisibilityHelpers do
   def visibility_badge(%{visibility: "public"}), do: nil
   def visibility_badge(subject) do
     human = Visibility.human_visibility subject.visibility
-    content_tag :span, class: "badge badge-primary" do
+    content_tag :span, class: "badge badge-default" do
       [
         content_tag(:i, "", class: "fa fa-fw fa-lock"),
         {:safe, [" "]},
