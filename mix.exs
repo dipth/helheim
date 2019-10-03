8fb1c2c51b89ad44ec297434afa6d1c41e7543f6
Thomas Dippel <thomas@dippel.dk>
2019-10-03 09:36:56 +0200

    use elixir 1.9.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Helheim.Mixfile do
     [
       app: :helheim,
       version: "0.0.2",
-      elixir: "~> 1.7.3",
+      elixir: "~> 1.9.1",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
