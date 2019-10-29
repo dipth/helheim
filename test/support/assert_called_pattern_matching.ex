34bfbb1ee3dea01e7731c0983b43e749a6eb8c8d
Thomas Dippel <thomas@dippel.dk>
2019-10-29 14:34:00 +0100

    improve AssertCalledPatternMatching

--- test/support/assert_called_pattern_matching.ex
+++ test/support/assert_called_pattern_matching.ex
@@ -4,23 +4,40 @@ defmodule Helheim.AssertCalledPatternMatching do
   using do
     quote do
       defp assert_called_with_pattern(entity, method, matcher) do
-        {_pid, {_entity, _method, args}, _result} = Enum.find(
-          :meck.history(entity),
-          fn({_, {_, m, _}, _}) -> m == method end
-        )
+        found = :meck.history(entity)
+          |> Enum.filter(fn({_, {_, m, _}, _}) -> m == method end)
+          |> Enum.any?(fn({_, {_, _, args}, _result}) ->
+            try do
+              matcher.(args)
+              true
+            rescue
+              MatchError -> false
+            end
+          end)
 
-        assert matcher.(args)
-      rescue
-        MatchError ->
-          calls = entity
-                |> :meck.history()
-                |> Enum.with_index()
-                |> Enum.map(fn {{_, {m, f, a}, ret}, i} ->
-                  "#{i}. #{m}.#{f}(#{a |> Enum.map(&Kernel.inspect/1) |> Enum.join(",")}) (returned #{inspect ret})"
-                end)
-                |> Enum.join("\n")
-          raise ExUnit.AssertionError,
-            message: "Expected call but did not receive it. Calls which were received:\n\n#{calls}"
+        case found do
+          true -> true
+          false ->
+            calls = received_calls(entity)
+            raise ExUnit.AssertionError,
+              message: "Expected call but did not receive it. Calls which were received:\n\n#{calls}"
+        end
+      end
+
+      defp refute_called_with_pattern(entity, method, matcher) do
+        assert_raise ExUnit.AssertionError, ~r/Expected call but did not receive it/, fn ->
+          assert_called_with_pattern(entity, method, matcher)
+        end
+      end
+
+      defp received_calls(entity) do
+        entity
+        |> :meck.history()
+        |> Enum.with_index()
+        |> Enum.map(fn {{_, {m, f, a}, ret}, i} ->
+          "#{i}. #{m}.#{f}(#{a |> Enum.map(&Kernel.inspect/1) |> Enum.join(",")}) (returned #{inspect ret})"
+        end)
+        |> Enum.join("\n")
       end
     end
   end
