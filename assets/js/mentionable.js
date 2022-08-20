3abd592e29851c3ca01232f6e04365585642b77d
Thomas Dippel <thomas@dippel.dk>
2022-08-20 14:25:33 +0200

    switch to Tribute for @-mentions

--- assets/js/mentionable.js
+++ assets/js/mentionable.js
@@ -1,17 +1,25 @@
+import Tribute from "../vendor/js/tribute";
+
 let regEx = /@([-\w\.\u00C0-\u017F]+)/giu;
 
 let replacer = function(match, username) {
   return `<a href="/usernames/${username}">${username}</a>`;
 }
 
-export var MentionableConfig = {
-  at: "@",
-  data: "/usernames"
+export var initializeTribute = function(element) {
+  console.log("initializeTribute called!", element)
+  let usernames = document.querySelector("meta[name=usernames]").getAttribute("content").split(",")
+  let tributes = usernames.map((username, _index) => ({ key: username, value: username }))
+  let tribute = new Tribute({
+    values: tributes,
+    menuItemLimit: 8
+  })
+  tribute.attach(element)
 }
 
 export var MentionableInput = {
   run: function(){
-    $('.mentionable-input').atwho(MentionableConfig);
+    initializeTribute(document.querySelectorAll(".mentionable-input"))
   }
 }
 
